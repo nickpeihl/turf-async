14829533b3dce51307bc2c7e100ea742ce58b455
Nick Peihl <nickp@sanjuanco.com>
2016-02-19 11:27:17 -0800

    Incorporate geobuf for passing geojson
    According to https://github.com/mapbox/mapbox-gl-js/issues/1504 the
    transferring of GeoJSON files between main and worker threads can cause
    a bottleneck. We should be able to reduce this bottleneck by converting
    and passing the GeoJSON file created in the Worker as a geobuf and
    decoding it on the main thread.
    
    This commit is broken because of a possible bug with Webworkify not
    pulling in correct dependencies. See
    https://github.com/substack/webworkify/issues/14

--- worker.js
+++ worker.js
@@ -1,36 +1,48 @@
 var concave = require('turf-concave');
 var geojsonhint = require('geojsonhint');
+var geobuf = require('geobuf');
+var Pbf = require('pbf');
 
+debugger;
 
 module.exports = function(self) {
-  self.addEventListener('message', function(ev) {
-    var dataUrl = ev.data[0];
-    self.makeRequest(dataUrl);
-  });
+    self.addEventListener('message', function(ev) {
+        var dataUrl = ev.data[0];
+        self.makeRequest(dataUrl);
+    });
 
-  self.makeRequest = function(url) {
-    var req = new XMLHttpRequest();
-    req.addEventListener('load', self.validateData);
-    req.addEventListener('error', self.transferFailed);
-    req.open('GET', url);
-    req.send();
-  };
+    self.makeRequest = function(url) {
+        var req = new XMLHttpRequest();
+        req.addEventListener('load', self.validateData);
+        req.addEventListener('error', self.transferFailed);
+        req.open('GET', url);
+        req.send();
+    };
 
-  self.validateData = function() {
-    var errors = geojsonhint.hint(this.responseText);
-    if (errors.len > 0) {
-      self.postMessage(['Errors', errors.join(', ') ]);
-    } else {
-      self.turfIt(this.responseText);
-    }
-  };
+    self.validateData = function() {
+        var errors = geojsonhint.hint(this.responseText);
+        if (errors.len > 0) {
+            self.postMessage(['Errors', errors.join(', ') ]);
+        } else {
+            self.turfIt(this.responseText);
+        }
+    };
 
-  self.transferFailed = function() {
-    self.postMessage(['Error', this.responseText]);
-  };
+    self.transferFailed = function() {
+        self.postMessage(['Error', this.responseText]);
+    };
 
-  self.turfIt = function(data) {
-    var results = concave(JSON.parse(data), 1, 'miles');
-    self.postMessage(['Done', results]);
-  };
+    self.turfIt = function(data) {
+        var results = concave(JSON.parse(data), 1, 'miles');
+        var pbf = new Pbf(geobuf.encode(results, new Pbf()));
+        /* The pbf does not contain the readFields method when called via
+         the web worker. However, I can set a breakpoint on the line below,
+         then rewrite the above line using a different variable (e.g. pbf2).
+         The pbf2 variable does contain the readFields method. I believe this
+         is due to the scope of the pbf buffer existing in the browser, but not
+         in the web worker. Possibly releated to this issue
+         (https://github.com/substack/webworkify/issues/14).
+        */
+        self.postMessage(['Done', pbf]);
+    };
 };
